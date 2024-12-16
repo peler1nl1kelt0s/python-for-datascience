ad5bcf6e6faa8f5e9dabd174d05a988a7b3e8614
Muhammed Esad Şeker <muhammed.seker@hepsiburada.com>
2024-12-17 00:38:12 +0300

    completed python-0

--- python-0/ex09/setup.py
+++ python-0/ex09/setup.py
@@ -3,14 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="ft_package",
     version="0.0.1",
+    summary="A sample test package",
     author="museker",
     author_email="museker@student.42istanbul.com.tr",
-    url="https://github.com/uxino/python-for-datascience/tree/main/python-0/ex09",
+    url="https://github.com/museker/ft_package",
     description="A sample test package",
-    packages=find_packages(),
-    classifiers=[],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     license="MIT",
-    entry_points={
-        'console_scripts': [],
-    },
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[],
 )
