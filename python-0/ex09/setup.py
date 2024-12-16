f6c2042b5adfd006628864c94d1168024f750129
Muhammed Esad Şeker <muhammed.seker@hepsiburada.com>
2024-12-16 23:39:16 +0300

    restore setup.py

--- python-0/ex09/setup.py
+++ python-0/ex09/setup.py
@@ -3,14 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="ft_package",
     version="0.0.1",
-    author="eagle",
-    author_email="eagle@42.fr",
-    url="https://github.com/eagle/ft_package",
+    author="museker",
+    author_email="museker@student.42istanbul.com.tr",
+    url="https://github.com/uxino/python-for-datascience/tree/main/python-0/ex09",
     description="A sample test package",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
+    entry_points={
+        'console_scripts': [],
+    },
 )
