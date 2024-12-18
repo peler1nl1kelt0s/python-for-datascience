10ef5db9e1511e9e0dae2c2ba975cbcbbf831263
Muhammed Esad Seker <museker@k1m05s04.42istanbul.com.tr>
2024-12-18 21:03:11 +0300

    python-1 ex02 completed

--- python-1/ex02/load_image.py
+++ python-1/ex02/load_image.py
@@ -1,4 +1,20 @@
 import numpy as np
+import cv2 as cv2
 
-def ft_load(path: str) -> array:
-    pass
+def ft_load(path: str) -> list:
+    try:
+        image = cv2.imread(path)
+        if image is None:
+            raise AssertionError("Wrong image path!")
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        array = np.array(rgb_image)
+        print("The shape of image is:",array.shape)
+        # print(array)
+        return f"{array[0, :3]}\n    ... \n {array[-1, -3:]}"
+    except AssertionError as msg:
+        print(AssertionError.__name__ + ":", msg)
+    except IOError as msg:
+        print(IOError.__name__ + ":", msg)
+    except Exception as e:
+        print(f"An unexpected error occurred: {str(e)}")
+
