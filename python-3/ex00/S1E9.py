1dc480b7a294a228302fb6eea370f6f0634adc16
Muhammed Esad Seker <museker@k1m30s06.42istanbul.com.tr>
2024-12-22 03:55:05 +0300

    python-3 started

---
+++ python-3/ex00/S1E9.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """Abstract class representing a character in a story."""
+
+    @abstractmethod
+    def __init__(self, name: str, is_alive : bool = True):
+        """
+        Constructor for Character.
+
+        :param first_name: The first name of the character.
+        :param is_alive: Boolean representing if the character is alive. Defaults to True.
+        """
+        self.name = name
+        self.is_alive = is_alive
+
+    def die(self):
+        """
+        Change the character's health state to deceased.
+
+        :return: None
+        """
+        self.is_alive = False
+
+class Stark(Character):
+    """A class representing a Stark family member."""
+    def __init__(self, name, is_alive = True):
+        """
+        Constructor for Stark.
+
+        :param first_name: The first name of the Stark family member.
+        :param is_alive: Boolean representing if the Stark is alive. Defaults to True.
+        """
+        super().__init__(name, is_alive)
---
+++ python-3/ex01/S1E7.py
@@ -0,0 +1,11 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+    def __init__(self, name, is_alive = True):
+        super().__init__(name, is_alive)
+    
+
+class Lannister(Character):
+    def __init__(self, name, is_alive = True):
+        super().__init__(name, is_alive)
+    
