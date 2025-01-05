3099b2972bfb7168d947f53abe11db912fb4b72a
Muhammed Esad Seker <museker@k1m04s06.42istanbul.com.tr>
2025-01-05 18:08:17 +0300

    python-3 is completed

--- python-3/ex04/ft_calculator.py
+++ python-3/ex04/ft_calculator.py
@@ -1,4 +1,11 @@
 class calculator:
+    """
+    Calculator class is to calculate a list.
+    Functions:
+        dotproduct : product whole list with another list
+        add_vec : sum whole list with another list
+        sous_vec : subtitude whole list with another list
+    """
     def dotproduct(V1: list[float], V2: list[float]) -> None:
         result = sum(list(V1[i] * V2[i] for i in range(len(V1))))
         print(f"Dot product is: {result}")
