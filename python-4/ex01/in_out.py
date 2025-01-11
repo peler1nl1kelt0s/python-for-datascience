00e23a0047b6aeca62111bb9b53c2b2cb4c33fee
Muhammed Esad Seker <museker@k1m43s01.42istanbul.com.tr>
2025-01-11 14:36:14 +0300

    python-4 ex01 completed

--- python-4/ex01/in_out.py
+++ python-4/ex01/in_out.py
@@ -1,10 +1,19 @@
 def square(x: int | float) -> int | float:
-    return x ** 2
+    x = x ** 2
+    return x
 
 def pow(x: int | float) -> int | float:
-    return 
+    return x ** x
+
 def outer(x: int | float, function) -> object:
     count = 0
     def inner() -> float:
-        return function(x)
+        nonlocal count
+        if count == 0:
+            result = function(x)
+        else:
+            result = function(inner.previus_result)
+        inner.previus_result = result
+        count += 1
+        return result
     return inner
